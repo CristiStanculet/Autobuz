2ea0d902bee6509237e7841d99d73785de4f2825
CristiStanculet <86767395+CristiStanculet@users.noreply.github.com>
2021-07-06 11:18:35 +0300

    06.07

--- Autobuz.js
+++ Autobuz.js
@@ -1,11 +1,20 @@
 Crafty.init(300,300, document.getElementById("game"));
 
-Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: 0, y: 250, w: 250, h: 10})
-  .color('green');
-
-Crafty.e('2D, Canvas, Color, Twoway, Gravity')
-  .attr({x: 0, y: 0, w: 50, h: 50})
+var autobuz= Crafty.e('2D, Canvas, Color, Twoway, AngularMotion, Motion')
+  .attr({x: 0, y: 0, w: 50, h: 20})
   .color('#F00')
-  .twoway(200)
-  .gravity('Floor');
+  .bind('KeyDown', function(e) {
+    if (e.key == Crafty.keys.LEFT_ARROW) {
+      this.x -= 1;
+    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
+      this.vx = 15*Math.cos(10);
+      this.vy = 15*Math.sin(10);
+      console.log(this.velocity());
+    } else if (e.key == Crafty.keys.UP_ARROW) {
+      this.y -= 1;
+    } else if (e.key == Crafty.keys.DOWN_ARROW) {
+      this.y += 1;
+    }
+  });
+  autobuz.rotation=10;
+  console.log(autobuz.rotation);
