240ee67e013762170212c531e420e147b7f3954d
CristiStanculet <86767395+CristiStanculet@users.noreply.github.com>
2021-07-21 11:33:04 +0300

    21.07

--- Autobuz.js
+++ Autobuz.js
@@ -1,20 +1,29 @@
 Crafty.init(300,300, document.getElementById("game"));
 
-var autobuz= Crafty.e('2D, Canvas, Color, Twoway, AngularMotion, Motion')
+var autobuz= Crafty.e('2D, Canvas, Color, AngularMotion, Motion')
   .attr({x: 0, y: 0, w: 50, h: 20})
   .color('#F00')
-  .bind('KeyDown', function(e) {
-    if (e.key == Crafty.keys.LEFT_ARROW) {
-      this.x -= 1;
-    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-      this.vx = 15*Math.cos(10);
-      this.vy = 15*Math.sin(10);
-      console.log(this.velocity());
-    } else if (e.key == Crafty.keys.UP_ARROW) {
-      this.y -= 1;
-    } else if (e.key == Crafty.keys.DOWN_ARROW) {
-      this.y += 1;
-    }
-  });
-  autobuz.rotation=10;
-  console.log(autobuz.rotation);
+  .bind('UpdateFrame', function() {
+  if (Crafty.s('Keyboard').isKeyDown('LEFT_ARROW')) {
+      var vel = this.velocity();
+      vel.x = 15*Math.cos(this.rotation*Math.PI/180);
+      vel.y = 15*Math.sin(this.rotation*Math.PI/180);
+      this.rotation -= 0.5;
+      console.log(this.rotation);
+      console.log(this.vx);
+};
+  if (Crafty.s('Keyboard').isKeyDown('RIGHT_ARROW')) {
+      var vel = this.velocity();
+      vel.x = 15*Math.cos(this.rotation*Math.PI/180);
+      vel.y = 15*Math.sin(this.rotation*Math.PI/180);
+      this.rotation += 0.5;
+      console.log(this.rotation);
+      console.log(this.vx);
+};
+  if (Crafty.s('Keyboard').isKeyDown('A')) {
+      var acc = this.acceleration();
+      acc.x = 50*Math.cos(this.rotation*Math.PI/180);
+      acc.y = 50*Math.sin(this.rotation*Math.PI/180);
+      console.log(this.ax);
+      console.log(this.vx);
+};});
