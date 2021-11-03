1d6b1c110fc5fcea4724474cce188c3d4d308259
CristiStanculet <86767395+CristiStanculet@users.noreply.github.com>
2021-11-03 11:12:25 +0200

    3.11

--- Autobuz.js
+++ Autobuz.js
@@ -1,29 +1,48 @@
-Crafty.init(300,300, document.getElementById("game"));
-
+Crafty.init(1000,1000, document.getElementById("game"));
+var v = 1;
 var autobuz= Crafty.e('2D, Canvas, Color, AngularMotion, Motion')
   .attr({x: 0, y: 0, w: 50, h: 20})
   .color('#F00')
   .bind('UpdateFrame', function() {
+  var viteza = v*this.velocity().magnitude();
   if (Crafty.s('Keyboard').isKeyDown('LEFT_ARROW')) {
       var vel = this.velocity();
-      vel.x = 15*Math.cos(this.rotation*Math.PI/180);
-      vel.y = 15*Math.sin(this.rotation*Math.PI/180);
+      vel.x = viteza*Math.cos(this.rotation*Math.PI/180);
+      vel.y = viteza*Math.sin(this.rotation*Math.PI/180);
       this.rotation -= 0.5;
-      console.log(this.rotation);
-      console.log(this.vx);
 };
   if (Crafty.s('Keyboard').isKeyDown('RIGHT_ARROW')) {
       var vel = this.velocity();
-      vel.x = 15*Math.cos(this.rotation*Math.PI/180);
-      vel.y = 15*Math.sin(this.rotation*Math.PI/180);
+      vel.x = viteza*Math.cos(this.rotation*Math.PI/180);
+      vel.y = viteza*Math.sin(this.rotation*Math.PI/180);
       this.rotation += 0.5;
-      console.log(this.rotation);
-      console.log(this.vx);
 };
-  if (Crafty.s('Keyboard').isKeyDown('A')) {
+  if (Crafty.s('Keyboard').isKeyDown('UP_ARROW')) {
       var acc = this.acceleration();
-      acc.x = 50*Math.cos(this.rotation*Math.PI/180);
-      acc.y = 50*Math.sin(this.rotation*Math.PI/180);
-      console.log(this.ax);
-      console.log(this.vx);
-};});
+      acc.x = 20*Math.cos(this.rotation*Math.PI/180);
+      acc.y = 20*Math.sin(this.rotation*Math.PI/180);
+};
+  if (Crafty.s('Keyboard').isKeyDown('DOWN_ARROW')) {
+      var acc = this.acceleration();
+      console.log(viteza);
+      if(viteza > 1) {
+      acc.x = -20*Math.cos(this.rotation*Math.PI/180);
+      acc.y = -20*Math.sin(this.rotation*Math.PI/180);
+      }
+      else {
+        var vel = this.velocity();
+        vel.x = 0;
+        vel.y = 0;
+        acc.x = 0;
+        acc.y = 0;
+      }};
+      if(Crafty.s('Keyboard').isKeyDown('B')) {
+      var acc = this.acceleration();
+      console.log(viteza);
+      if(viteza == 0) {
+        acc.x = -20*Math.cos(this.rotation*Math.PI/180);
+        acc.y = -20*Math.sin(this.rotation*Math.PI/180);
+        console.log(viteza);
+        }
+};
+});
